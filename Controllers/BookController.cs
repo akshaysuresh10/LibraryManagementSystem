0dfe55cda2f9b76036e085d492f98bf653ba8b02
vishnupc <66262226+vishnupc0016@users.noreply.github.com>
2020-07-26 17:40:29 +0530

    Bookadd

---
+++ Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            return RedirectToAction("AdminLogin");
+        }
+
+        public IActionResult AdminLogin()
+        {
+            return View();
+        }
+
+       [HttpPost]
+        public IActionResult Loginpage(String  Username,String Password)
+        {
+            if (Username == "Admin" && Password == "1234")
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("AdminLogin");
+            }
+            return View();
+            
+        }
+        
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace LibraryManagementSystem
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Book}/{action=AdminLogin}/{id?}");
             });
         }
     }
