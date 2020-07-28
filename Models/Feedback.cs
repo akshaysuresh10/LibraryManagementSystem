08915aacc55f3fb3f6903790fdc78b9fe564d62e
Akshay Suresh <Akshay Suresh@DESKTOP-DIBS5M1>
2020-07-28 11:59:46 -0700

    templatechanges

---
+++ Models/Feedback.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Feedback
+    {
+        public int Fid { get; set; }
+        public String Message { get; set; }
+    }
+}
